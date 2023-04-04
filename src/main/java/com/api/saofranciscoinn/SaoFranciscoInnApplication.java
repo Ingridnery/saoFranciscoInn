2afe42c5981534ac0a0957c592ff4ecb6dce775d
IngridNery <neryingrid16@gmail.com>
2023-04-04 08:58:09 -0300

    Initial commit

---
+++ src/main/java/com/api/saofranciscoinn/SaoFranciscoInnApplication.java
@@ -0,0 +1,13 @@
+package com.api.saofranciscoinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaoFranciscoInnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaoFranciscoInnApplication.class, args);
+    }
+
+}
